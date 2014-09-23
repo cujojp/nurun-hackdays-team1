0f8ee872a2b72bb8dc28f61466c443ae5d158f67
Brian Kenny <brian.kenny@nurun.com>
2014-09-23 10:47:41 -0700

    init

---
+++ digihood/DigiHood/DigiHood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DigiHood
+//
+//  Created by Brian Kenny on 9/23/14.
+//  Copyright (c) 2014 nurun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
