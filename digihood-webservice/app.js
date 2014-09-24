8de6fbecd10f6d261580083d304351fa8b1d089c
Kaleb White <info@cujo.jp>
2014-09-24 16:35:41 -0700

    adding app.js

--- digihood-webservice/app.js
+++ digihood-webservice/app.js
@@ -1,1 +1,15 @@
-web: node app.js
+var port = process.env.PORT || 9000,
+	auth = require('http-auth'),
+	compression = require('compression'),
+	basic = auth.basic({
+    	realm: "Blu Homes Protected area",
+    	file: __dirname + "/.htpasswd" 
+	}),
+	express = require('express'),
+	app = express()
+		.use(auth.connect(basic))
+		.use(compression())
+		.use(express.static('dist'))
+		.listen(port);
+
+console.log('Started express web server on port ' + port);
